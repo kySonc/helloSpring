4bdf9c36b720f84d4b586826e8d6a2a9d6dde0a9
kySonc <dlswhsqhr@gmail.com>
2020-12-06 16:01:08 +0900

    @Autowired(required = false) 사용하기

--- src/spring/di/ui/InlineExamConsole.java
+++ src/spring/di/ui/InlineExamConsole.java
@@ -7,26 +7,32 @@ import spring.di.entity.Exam;
 
 public class InlineExamConsole implements ExamConsole {
 	
+
+	@Autowired(required = false)
+	@Qualifier("exam2")
 	private Exam exam;
 	
 	public InlineExamConsole() {
-		// TODO Auto-generated constructor stub
+		System.out.println("constructor"); //필드로 @Autowired를 사용할때 기본생성자가 호출되는지 확인 
 	}
 	
 	public InlineExamConsole(Exam exam) {
+		System.out.println("overloaded constructor"); 
 		this.exam = exam;
 	}
 
 	@Override
 	public void print() {
-		System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
+		if(exam == null)
+			System.out.printf("total is %d, avg is %f\n", 0, 0.0);
+		else
+			System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
 
 	}
 	
-	@Autowired
-	@Qualifier("exam2")
 	@Override
 	public void setExam(Exam exam) {
+		System.out.println("setter"); //필드로 @Autowired를 사용할때 호출되는지 확인
 		this.exam = exam;
 		
 	}
