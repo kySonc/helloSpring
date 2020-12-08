be31d47840164fee79f00318180b84865042a6c2
kySonc <dlswhsqhr@gmail.com>
2020-12-08 15:15:09 +0900

    스프링 AOP로 AroundAdvice 구현하기

--- src/spring/aop/Program.java
+++ src/spring/aop/Program.java
@@ -4,40 +4,52 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import spring.aop.entity.Exam;
 import spring.aop.entity.KysoncExam;
+import spring.di.KysoncDIConfig;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Exam exam = new KysoncExam(1,1,1,1);
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring/aop/setting.xml"); //Xml을 이용
+		//ApplicationContext context = new AnnotationConfigApplicationContext(KysoncDIConfig.class); //어노테이션을 이용
 		
-		//Proxy.newProxyInstance(loader, interfaces, h)
-		Exam proxy = (Exam) Proxy.newProxyInstance(KysoncExam.class.getClassLoader(),
-				new Class[] {Exam.class},
-				new InvocationHandler() {
-					
-					@Override
-					public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-						
-						long start = System.currentTimeMillis();
-						
-						Object result = method.invoke(exam, args);
-																		
-						long end = System.currentTimeMillis();
-						
-						String massage = (end-start) + "ms 시간이 걸렸습니다";
-						System.out.println(massage);
-						
-						return result;
-					}
-				}
-		);
+		
+		Exam exam = (Exam) context.getBean("exam");
+		
 		//System.out.printf("total is %d\n", exam.total());
-		System.out.printf("total is %d\n", proxy.total());
+		System.out.printf("total is %d\n", exam.total());
 		//System.out.printf("avg is %f\n", exam.avg());
-		System.out.printf("avg is %f\n", proxy.avg());
+		System.out.printf("avg is %f\n", exam.avg());
+		
+		/*
+		 * Exam exam = new KysoncExam(1,1,1,1);
+		 * 
+		 * 
+		 * //Proxy.newProxyInstance(loader, interfaces, h) Exam proxy = (Exam)
+		 * Proxy.newProxyInstance(KysoncExam.class.getClassLoader(), new Class[]
+		 * {Exam.class}, new InvocationHandler() {
+		 * 
+		 * @Override public Object invoke(Object proxy, Method method, Object[] args)
+		 * throws Throwable {
+		 * 
+		 * long start = System.currentTimeMillis();
+		 * 
+		 * Object result = method.invoke(exam, args);
+		 * 
+		 * long end = System.currentTimeMillis();
+		 * 
+		 * String massage = (end-start) + "ms 시간이 걸렸습니다"; System.out.println(massage);
+		 * 
+		 * return result; } } );
+		 */ 
+			 
+		 
 	}
 
 }
---
+++ src/spring/aop/advice/LogAroundAdvice.java
@@ -0,0 +1,23 @@
+package spring.aop.advice;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+public class LogAroundAdvice implements MethodInterceptor{
+
+	@Override
+	public Object invoke(MethodInvocation invocation) throws Throwable {
+				
+		long start = System.currentTimeMillis();
+		
+		Object result = invocation.proceed(); //주업무 호출
+		
+		long end = System.currentTimeMillis();
+		
+		String massage = (end-start) + "ms 시간이 걸렸습니다";
+		System.out.println(massage);
+		
+		return result;
+	}
+
+}
